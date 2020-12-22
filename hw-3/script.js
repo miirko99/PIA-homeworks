3e4ddbcad3f7fbb007c0fe0b9806a0451ab7da0c
miirko99 <66567615+miirko99@users.noreply.github.com>
2020-12-22 11:35:14 +0100

    Update script.js

--- hw-3/script.js
+++ hw-3/script.js
@@ -1,12 +1,13 @@
 var holder=document.getElementById("holder");
 window.addEventListener("unload",()=>{localStorage.setItem("scores",JSON.stringify(scores))})
-window.addEventListener("load",()=>{let niz=JSON.parse(localStorage.getItem("scores"))
-					if(niz!=null){
-						for(let clan of niz){
-							scores.push(new rezultat(clan.ime,clan.poeni))
-						}
-					}
-				})
+window.addEventListener("load",()=>{
+								let niz=JSON.parse(localStorage.getItem("scores"))
+								if(niz!=null){
+									for(let clan of niz){
+										scores.push(new rezultat(clan.ime,clan.poeni))
+									}
+								}
+						})
 pocetniEkran()
 var pitanja
 fetch('questions.json')
@@ -22,9 +23,9 @@ var scores=[]
 function pocetniEkran(){
 	holder.innerHTML=`<p id="imeKviza">KVIZ</p>
 		<label for="inputIme" id="labelIme">Unesite vase ime</label>
-    	<input type='text' id='inputIme' placeholder='Vase ime?'></br>
+    	<input type='text' id='inputIme' placeholder='Vase ime?' autocomplete='off'></br>
 		<button onclick='potvrdi()' class='btn' id='btnPotvrdi'>Potvrdi</button>
-		<button onclick='prikaziRezultat()' class='btn' id='btnRezultati'>Lista rezultata</button>`
+		<button onclick='prikaziRezultate()' class='btn' id='btnRezultati'>Lista rezultata</button>`
 	iskorisceniIndeksi=[]
 	poeni=0
 	ime=""
@@ -40,7 +41,7 @@ function potvrdi(){
 	}
 }
 function prikaziPravila(){
-	holder.innerHTML=`<h1>Pravila kviza</h1><p>Pravila kviza su jednostavna, dobijate 10 pitanja za neka cete dobiti ponudjene odgovore, za neka ne.svako pitanje vredi 1 poen negativnih poena nema. Za svako pitanje imate tacno 20s da odgovorite, ako ne odgovorite prelazi se na sledece pitanje. Pritiskom na dugme dalje pitanje se preskace i ne mozete se vratiti na to pitanje.</br> Srecno.</p><button onclick='zapocniIgru()' id='btnZapocni' class='btn'>Pocni kviz</button>`
+	holder.innerHTML=`<h1>Pravila kviza</h1><p>Pravila kviza su jednostavna, dobijate 10 pitanja za neka cete dobiti ponudjene odgovore, za neka ne. Svako pitanje vredi 1 poen negativnih poena nema. Za svako pitanje imate tacno 20s da odgovorite, ako ne odgovorite prelazi se na sledece pitanje. Pritiskom na dugme dalje pitanje se preskace i ne mozete se vratiti na to pitanje. U svakom trenutku mozete da odustanete ali vas rezultat ce biti sacuvan</br> Srecno.</p><button onclick='zapocniIgru()' id='btnZapocni' class='btn'>Pocni kviz</button>`
 }
 function zapocniIgru(){
 	holder.innerHTML="<div id='QIA'></div>"
@@ -66,10 +67,10 @@ function sledecePitanje(){
 	QIA.innerHTML=""
 	indexPitanja=randomIndeks()
 	if(indexPitanja==-1){
-		alert(ime+" vas rezultat je "+poeni+" poena.")
 		var rez=new rezultat(ime,poeni)
 		scores.push(rez)
-		pocetniEkran()
+		prikaziRezultat()
+		
 		return
 	}
 	var pitanje=pitanja[indexPitanja]
@@ -119,6 +120,16 @@ function sledecePitanje(){
 	d.classList.add("btn")
 	d.innerHTML="Preskoci"
 	d.onclick=proveriOdg
+	QIA.appendChild(d)
+
+	d=document.createElement("button")
+	d.classList.add("btn")
+	d.innerHTML="Odustani"
+	d.onclick=()=>{
+		scores.push(new rezultat(ime,poeni))
+		pocetniEkran()
+		clearInterval(interval)
+	}
 	QIA.appendChild(d)
 	
 	
@@ -174,7 +185,7 @@ function potvrdiOdg(){
 	setTimeout(sledecePitanje,3000)
 	clearInterval(interval)
 }
-function prikaziRezultat(){
+function prikaziRezultate(){
 	list=document.createElement("ol")
 	holder.innerHTML=""
 	holder.appendChild(list)
@@ -187,7 +198,8 @@ function prikaziRezultat(){
 	}
 	btn=document.createElement("button")
 	btn.classList.add("btn")
-	btn.innerText="Meny"
+	btn.id="menu"
+	btn.innerText="Menu"
 	btn.onclick=pocetniEkran
 	holder.appendChild(btn)
 	//dodati dugme za meni
@@ -222,4 +234,29 @@ function sortitajRezultate(niz){
 			}
 		}
 	}
+	while(scores.length>10) scores.pop()
+}
+function prikaziRezultat(){
+	holder.innerHTML=''
+	e=document.createElement("div")
+	e.id="rezultat"
+	e.innerText="Igra je gotova vas rezultat je "+poeni+" poena."
+	holder.appendChild(e)
+
+	e=document.createElement("button")
+	e.classList.add("btn")
+	e.id="menu"
+	e.innerText="Menu"
+	e.onclick=pocetniEkran
+	holder.appendChild(e)
+	e=document.createElement("button")
+	e.classList.add("btn")
+	e.id="retry"
+	e.innerText="Retry"
+	e.onclick=()=>{
+		iskorisceniIndeksi=[]
+		poeni=0
+		zapocniIgru()
+	}
+	holder.appendChild(e)
 }
