ed5a10d7cab6944d1815e341c40c453e27033f8d
miirko99 <66567615+miirko99@users.noreply.github.com>
2021-01-31 23:33:59 +0100

    Update filmovi.php

--- hw-4/filmovi.php
+++ hw-4/filmovi.php
@@ -60,8 +60,47 @@ switch($func){
 
     $db->query($sql);
     echo "$naslov je obrisan";
-    
     break;
+  case "getNamePhoto":
+    $sql="select * from films";
+    $filmovi=$db->query($sql);
+    $niz=[];
+    foreach($filmovi as $f){
+      array_push($niz,json_encode(array("naslov"=> $f["naslov"],"slika"=>$f["slika"], "id"=>$f["filmID"])));
+    }
+    echo json_encode($niz);
+    break;
+  case "getById":
+    $id=$data["id"];
+    $film=$db->query("select * from films where filmID =$id");
+    $pocena=$db->query("select avg(ocena) as av from ocene where filmID=$id");
+    $po;
+    foreach($pocena as $p){
+      $po=$p["av"];
+    }
+    $cocena=$db->query("select count(ocena) as c from ocene where filmID=$id");
+    $co;
+    foreach($cocena as $c){
+      $co=$c["c"];
+    }
+    foreach($film as $f){
+      
+      echo json_encode(array("naslov"=> $f["naslov"], "opis"=> $f["opis"], "zanr"=> $f["zanr"], "scenarista"=> $f["scenarista"], "reziser"=> $f["reziser"], "prodKuca"=> $f["prodKuca"], "godina"=> $f["godina"], "slika"=> $f["slika"], "trajanje"=> $f["trajanje"], "glumci"=> $f["glumci"], "id" => $f["filmID"], "pOcena"=>$po, "cOcena"=>$co));
+    }
+    break;
+  case "find":
+    $pretraga=$data["pretraga"];
+    $tZanrovi=$data["zanrovi"];//trazeni zanrovi
+    $sql="select * from films where naslov like '%$pretraga%'";
+    $niz=[];
+    $filmovi=$db->query($sql);
+    foreach($filmovi as $film){
+      array_push($niz,json_encode(array("naslov"=>$film["naslov"], "zanrovi"=>$film["zanr"], "id"=>$film["filmID"],"slika"=>$film["slika"])));
+    }
+    echo json_encode($niz);
+    break;
+
+
 };
 
 
